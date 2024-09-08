4e6bfa1916c628e78ae51a3b4fede93ca0b931d9
santiagoCorreac1 <santiago.correac1@udea.edu.co>
2024-09-07 19:36:46 -0500

    gender mapper added

--- src/main/java/com/authb/api_auth/mapper/GenderMapper.java
+++ src/main/java/com/authb/api_auth/mapper/GenderMapper.java
@@ -1,7 +1,22 @@
 package com.authb.api_auth.mapper;
 
+import com.authb.api_auth.dto.GenderDto;
+import com.authb.api_auth.entity.Gender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GenderMapper {
+
+    public static GenderDto toGenderDto(Gender gender){
+        return new GenderDto(
+                gender.getId(),
+                gender.getName()
+        );
+    }
+    public static Gender toGender(GenderDto genderDto){
+        return new Gender(
+                genderDto.getId(),
+                genderDto.getGender()
+        );
+    }
 }
